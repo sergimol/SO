617286c6119a16a832601e994923c772f415480e
sergimol <sergimol@ucm.es>
2022-10-20 12:37:08 +0200

    alubias

--- ficheros_p2/ejercicio4/mostrar.c
+++ ficheros_p2/ejercicio4/mostrar.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int N = 0;
 
 int main(int argc, char *argv[])
 {
+	if(getopt(argc, argv, "n:") != -1){
+		N = optarg - '0';
+		if(getopt(argc, argv, "e") != -1){
+			
+		}
+		else{
 
+		}
+	}
+	
 	return 0;
 }
